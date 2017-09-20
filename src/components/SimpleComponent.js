210fcbd60197be9681543e2cf67e456c3bb78ac3
bother7 <github email address>
2017-09-20 13:48:43 -0400

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,24 @@
 // Code SimpleComponentHere Here
+import React, {Component} from 'react'
+
+export default class SimpleComponent extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      mood: "happy"
+    }
+  }
+
+  toggle = () => {
+    if (this.state.mood === "happy") {
+      this.setState({mood: "sad"})
+    } else {
+    this.setState({mood: "happy"})
+    }
+  }
+
+  render () {
+    return (<div onClick={this.toggle}>{this.state.mood}</div>)
+  }
+}
